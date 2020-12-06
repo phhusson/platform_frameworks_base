d3a7ce0e4ab2fb7a53ed017a721bacfc5bb63196
Pierre-Hugues Husson <phh@phh.me>
2020-12-06 12:20:08 +0100

    Make rounded corners padding overridable with persist.sys.phh.rounded_corners_padding

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -25,6 +25,7 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.os.SystemProperties;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.util.TypedValue;
@@ -189,8 +190,10 @@ public class KeyguardStatusBarView extends RelativeLayout
                 R.dimen.display_cutout_margin_consumption);
         mShowPercentAvailable = getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_battery_percentage_setting_available);
-        mRoundedCornerPadding = res.getDimensionPixelSize(
-                R.dimen.rounded_corner_content_padding);
+        mRoundedCornerPadding = SystemProperties.getInt("persist.sys.phh.rounded_corners_padding", -1);
+        if(mRoundedCornerPadding == -1)
+            mRoundedCornerPadding = res.getDimensionPixelSize(
+                    R.dimen.rounded_corner_content_padding);
     }
 
     private void updateVisibilities() {
